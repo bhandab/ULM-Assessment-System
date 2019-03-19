1b8d2b275acdcc26c58fbb56acf49fb896aa3d9a
Bidhan Bhandari <bhandariprachhanna@gmail.com>
2019-03-18 23:03:12 -0500

    Add concurrently in server script, server running on port 5000, client running on port 3000

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/users',userRouter)
 app.use('/api/outcomes',outcomeRouter)
 app.use('/api/measures',measureRouter)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port,()=>{
     console.log('Server listening to port ', port);
