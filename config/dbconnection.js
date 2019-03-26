d58ed13beea9994c14bd79817fd801b51bb355c6
Bidhan Bhandari <bhandariprachhanna@gmail.com>
2019-03-26 18:26:20 -0500

    DB connection update

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,13 +1,25 @@
 const mysql = require('mysql')
 require('dotenv').config()
+let connection = null;
+if(process.env.NODE_ENV === 'production'){
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password:process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+        
+    })
+}
+else{
+    connection = mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        password:"",
+        database:"ASSESSMENT_ASSIST"
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-    
-})
+    })
+}
+
 connection.connect((error)=>{
     if(error){
         return console.log('Error connecting to database! ', error)
