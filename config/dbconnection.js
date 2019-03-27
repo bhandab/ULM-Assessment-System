6a5ab2035fad10280fa6f8b59e3e8f34f7c50928
Bidhan Bhandari <bhandariprachhanna@gmail.com>
2019-03-26 19:24:56 -0500

    Deploy in Progress]

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,22 +1,22 @@
 const mysql = require("mysql");
-require("dotenv").config();
-//let connection = null;
-//if (process.env.NODE_ENV === "production") {
-let connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-//}
-// else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "ASSESSMENT_ASSIST"
-//   });
-// }
+
+let connection = null;
+if (process.env.NODE_ENV === "production") {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+} else {
+  require("dotenv").config();
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+}
 
 connection.connect(error => {
   if (error) {
--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,10 @@
-require("dotenv").config();
-
-//if (process.env.NODE_ENV === "production") {
-module.exports = {
-  secretOrkey: process.env.SECRET
-};
-//}
-// else {
-//   module.exports = {
-//     secretOrkey: "secret"
-//   };
-// }
+if (process.env.NODE_ENV === "production") {
+  module.exports = {
+    secretOrkey: process.env.SECRET
+  };
+} else {
+  require("dotenv").config();
+  module.exports = {
+    secretOrkey: process.env.SECRET
+  };
+}
