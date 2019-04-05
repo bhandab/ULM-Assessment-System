858e164a1f8849aa6389bf7a1740722f5ffa10f4
Bidhan Bhandari <bhandariprachhanna@gmail.com>
2019-04-05 16:17:21 -0500

    Minor CSS changes by angel during class; minor changes in config files

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,33 +1,17 @@
 const mysql = require("mysql");
-
-let connection = null;
-
-if (process.env.NODE_ENV === "production") {
-  connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  });
-} else {
-  require("dotenv").config();
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "ASSESSMENT_ASSIST"
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME
-  });
-}
+require("dotenv").config();
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
 connection.connect(error => {
   if (error) {
     return console.log("Error connecting to database! ", error);
   }
-  console.log("MySQL connection Successful!");
+  console.log("MySQL connection Successful!", connection);
 });
 
 module.exports = connection;
--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,4 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    secretOrkey: process.env.SECRET
-  };
-} else {
-  require("dotenv").config();
-  module.exports = {
-    secretOrkey: process.env.SECRET
-  };
-}
+require("dotenv").config();
+module.exports = {
+  secretOrkey: process.env.SECRET
+};
