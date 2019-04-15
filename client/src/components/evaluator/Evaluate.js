5916ad95cfc1937b6e890404ea041faa273595de
Angel Gyawali <angel.gyawali@gmail.com>
2019-04-14 21:42:42 -0500

    working on evaluations

--- client/src/components/evaluator/Evaluate.js
+++ client/src/components/evaluator/Evaluate.js
@@ -8,30 +8,45 @@ import { ListGroup, Card } from "react-bootstrap";
 
 class Evaluate extends Component {
 
+    state = {
+        rubric : "Select A Student",
+        stud: ""
+    }
+
     componentDidMount(){
         this.props.getEvaluatorDetails()
     }
 
 
     onClickListener = e => {
-        console.log("Clicked!!!!")
+        const index = e.target.dataset.idx
+        let student = this.props.evaluations.evaluationDetails[index]
+
+
     }
     render(){
-        let studentList = []
-        if (this.props.evaluations.evaluationDetails !== null) {
-            studentList = this.props.evaluations.evaluationDetails.map( (student, index) => {
-                return(
-                    <ListGroup.Item className="list-group-item" value={index}>{student.studentName} | {student.rubricName}</ListGroup.Item>
-                )
-            })
-        }
+    //     let studentList = []
+    //     let studSet = new Set()
+    //    /* if (this.props.evaluations.evaluationDetails !== null) {
+    //         const studentArray = this.props.evaluations.evaluationDetails
+    //         /*studentList =*/ studentArray .map( (student, index) => {
+    //             if(!studSet.has(student.rubricName)){
+    //                 studSet.add(student.rubricName)
+    //             }
+    //             /*return(
+    //                 <ListGroup.Item className="list-group-item" key={index} data-idx={index} onClick={(e)=>this.onClickListener(e)}>{student.studentName} | {student.rubricName}</ListGroup.Item>
+    //             )*/
+    //             //studentArray.
+    //         })
+    //     }
+    //     console.log(studSet)
 
         console.log(this.props)
         return(
             <section className="panel important">
             <p>Evaluator</p>
             <Card className="row">
-            <ListGroup className="students col-3">{studentList}</ListGroup>
+            <ListGroup className="students col-3">studentList</ListGroup>
             </Card>
             </section>
         )
