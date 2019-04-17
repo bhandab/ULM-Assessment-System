26d5a90b28bc82ee965c076476a4634e005700d9
Bidhan Bhandari <bhandaribidhan7@gmail.com>
2019-04-17 12:04:30 -0500

    evaluate api issue solved

--- routes/api/evaluations.js
+++ routes/api/evaluations.js
@@ -90,55 +90,79 @@ router.post(
       } else if (result.length <= 0) {
         return res.status(404).json("Rubric Not Found");
       }
-      let weighted = result[0].weighted;
-      console.log(weighted);
-      let sql1 = "SELECT * FROM CRITERIA WHERE toolID=" + db.escape(rubricID);
+
+      let sql1 =
+        "SELECT * FROM EVALUATE WHERE toolID=" +
+        db.escape(rubricID) +
+        " AND studentID=" +
+        db.escape(studentID) +
+        " AND measureEvalID=" +
+        db.escape(measureEvalID);
       db.query(sql1, (err, result) => {
         if (err) {
           return res.status(500).json(err);
-        }
-        result.forEach(row => {
-          let weight = null;
-          if (weighted) {
-            weight = row.criteriaWeight;
-          }
-          let tempCriteria = {
-            criteriaWeight: weight,
-            criteriaID: row.criteriaID
-          };
-          criteriaScores.push(tempCriteria);
-        });
-        console.log(criteriaScores);
-        criteriaScores.forEach(score => {
-          let cScore = 1;
-          if (score.criteriaWeight) {
-            cScore = cScore * score.criteriaWeight;
-          }
-          let criteriaScore = [
-            rubricID,
-            measureID,
-            score.criteriaID,
-            studentID,
-            measureEvalID,
-            cScore
-          ];
-          evaluatedScores.push(criteriaScore);
+        } else if (result.length > 0) {
+          result.forEach(row => {
+            let indEval = {
+              criteriaID: row.criteriaID,
+              criteriaScore: row.criteriaScore
+            };
+            evalInfo.push(indEval);
+          });
+          return res.status(200).json(evalInfo);
+        } else {
+          let weighted = result[0].weighted;
+          console.log(weighted);
+          let sql2 =
+            "SELECT * FROM CRITERIA WHERE toolID=" + db.escape(rubricID);
+          db.query(sql2, (err, result) => {
+            if (err) {
+              return res.status(500).json(err);
+            }
+            result.forEach(row => {
+              let weight = null;
+              if (weighted) {
+                weight = row.criteriaWeight;
+              }
+              let tempCriteria = {
+                criteriaWeight: weight,
+                criteriaID: row.criteriaID
+              };
+              criteriaScores.push(tempCriteria);
+            });
+            console.log(criteriaScores);
+            criteriaScores.forEach(score => {
+              let cScore = 1;
+              if (score.criteriaWeight) {
+                cScore = cScore * score.criteriaWeight;
+              }
+              let criteriaScore = [
+                rubricID,
+                measureID,
+                score.criteriaID,
+                studentID,
+                measureEvalID,
+                cScore
+              ];
+              evaluatedScores.push(criteriaScore);
 
-          let indEval = {
-            criteriaID: score.criteriaID,
-            criteriaScore: cScore
-          };
-          evalInfo.push(indEval);
-        });
-        let sql2 = "";
-        let sql3 =
-          "INSERT INTO EVALUATE (toolID,measureID,criteriaID,studentID,measureEvalID,criteriaScore) VALUES ?";
-        db.query(sql3, [evaluatedScores], (err, result) => {
-          if (err) {
-            return res.status(200).json(evalInfo);
-          }
-          res.status(200).json(evalInfo);
-        });
+              let indEval = {
+                criteriaID: score.criteriaID,
+                criteriaScore: cScore
+              };
+              evalInfo.push(indEval);
+            });
+
+            let sql3 =
+              "INSERT INTO EVALUATE (toolID,measureID,criteriaID,studentID,measureEvalID,criteriaScore) VALUES ?";
+            db.query(sql3, [evaluatedScores], (err, result) => {
+              if (err) {
+                return res.status(500).json(err);
+              }
+              res.status(200).json(evalInfo);
+            });
+          });
+        }
       });
     });
   }
