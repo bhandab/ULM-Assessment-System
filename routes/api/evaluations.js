1fee351fc7446f8147e9d6b1d4cf6df7f0eb4910
Bidhan Bhandari <bhandaribidhan7@gmail.com>
2019-04-17 00:33:44 -0500

    Updated evluate rubric

--- routes/api/evaluations.js
+++ routes/api/evaluations.js
@@ -82,45 +82,63 @@ router.post(
     let measureEvalID = req.body.measureEvalID;
     let evaluatedScores = [];
     let evalInfo = [];
-
+    let criteriaScores = [];
     let sql0 = "SELECT * FROM RUBRIC WHERE toolID=" + db.escape(rubricID);
     db.query(sql0, (err, result) => {
       if (err) {
         return res.status(500).json(err);
       } else if (result.length <= 0) {
         return res.status(404).json("Rubric Not Found");
-      } else if (req.body.criteriaScores.length !== result[0].rubricRows) {
-        return res.status(404).json("Please Grade all criterias of the rubric");
       }
-      req.body.criteriaScores.forEach(score => {
-        let cScore = 1;
-        if (score.criteriaWeight) {
-          cScore = (cScore * parseFloat(criteriaWeight)) / 100;
-        }
-        let criteriaScore = [
-          rubricID,
-          measureID,
-          score.criteriaID,
-          studentID,
-          measureEvalID,
-          cScore
-        ];
-        evaluatedScores.push(criteriaScore);
-
-        let indEval = {
-          criteriaID: score.criteriaID,
-          criteriaScore: cScore
-        };
-        evalInfo.push(indEval);
-      });
-
-      let sql1 =
-        "INSERT INTO EVALUATE (toolID,measureID,criteriaID,studentID,measureEvalID,criteriaScore) VALUES ?";
-      db.query(sql1, [evaluatedScores], (err, result) => {
+      let weighted = result[0].weighted;
+      console.log(weighted);
+      let sql1 = "SELECT * FROM CRITERIA WHERE toolID=" + db.escape(rubricID);
+      db.query(sql1, (err, result) => {
         if (err) {
           return res.status(500).json(err);
         }
-        res.status(200).json(evalInfo);
+        result.forEach(row => {
+          let weight = null;
+          if (weighted) {
+            weight = row.criteriaWeight;
+          }
+          let tempCriteria = {
+            criteriaWeight: weight,
+            criteriaID: row.criteriaID
+          };
+          criteriaScores.push(tempCriteria);
+        });
+        console.log(criteriaScores);
+        criteriaScores.forEach(score => {
+          let cScore = 1;
+          if (score.criteriaWeight) {
+            cScore = cScore * score.criteriaWeight;
+          }
+          let criteriaScore = [
+            rubricID,
+            measureID,
+            score.criteriaID,
+            studentID,
+            measureEvalID,
+            cScore
+          ];
+          evaluatedScores.push(criteriaScore);
+
+          let indEval = {
+            criteriaID: score.criteriaID,
+            criteriaScore: cScore
+          };
+          evalInfo.push(indEval);
+        });
+
+        let sql2 =
+          "INSERT INTO EVALUATE (toolID,measureID,criteriaID,studentID,measureEvalID,criteriaScore) VALUES ?";
+        db.query(sql2, [evaluatedScores], (err, result) => {
+          if (err) {
+            return res.status(500).json(err);
+          }
+          res.status(200).json(evalInfo);
+        });
       });
     });
   }
@@ -143,8 +161,6 @@ router.post(
         return res.status(500).json(err);
       } else if (result.length <= 0) {
         return res.status(404).json("Rubric Not Found");
-      } else if (req.body.criteriaScores.length !== result[0].rubricRows) {
-        return res.status(404).json("Please Grade all criterias of the rubric");
       }
 
       async.forEachOf(
