b8caf2d554f40189c045c9abfd039a119d86eacb
Suvash <suvashacha@gmail.com>
2019-04-16 23:56:23 -0500

    added styles from origin master

--- client/src/components/layouts/AdminLayout.js
+++ client/src/components/layouts/AdminLayout.js
@@ -21,26 +21,56 @@ class AdminLayout extends Component {
     console.log("Logout user!");
     this.props.logoutUser();
   };
+
+  /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+
   
   render() {
+
+    // function openNav() {
+    //   document.getElementById("mySidebar").style.width = "250px";
+    //   // document.getElementById("main").style.marginLeft = "250px";
+    //   document.getElementById("disappear").style.display = "none";
+    
+    // }
+    
+    // function closeNav() {
+    //   document.getElementById("mySidebar").style.width = "0";
+    //   // document.getElementById("main").style.marginLeft = "0";
+    //   document.getElementById("disappear").style.display = "inline";
+    
+    // }
+
+    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+
     return (
       <Fragment>
         <header>
-          <h1>Admin Panel</h1>
-          <ul className="utilities">
-            <li className="users">
-              <Link to="#">{this.props.auth.user.name}</Link>
-            </li>
-            <li className="logout warn">
-              <Link to="/login" onClick={this.onLogoutClick.bind(this)}>
-                Log Out
-              </Link>
-            </li>
-          </ul>
-        </header>
+        <h1 className = "justify-content-between">
+          <button id = "disappear" class="openbtn" onClick={openNav}>&#9776; </button>
+           <span> Admin Panel</span>
+        </h1>
+        {/* <div id="main">
 
-        <nav>
-          <ul className="main">
+       
+
+
+        </div> */}
+           
+          <div id="mySidebar" className="sidebar" style={{backgroundColor:'grey'}}>
+            <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&#9776;</a>
+           <ul className="bordered m-3"style={{backgroundColor:'white'}}>
             <li className="dashboard">
               <Link to="/admin/cycles">Dashboard</Link>
             </li>
@@ -59,8 +89,25 @@ class AdminLayout extends Component {
             <li className="evaluators">
             <Link to="/admin/evaluators">Evaluators</Link>
             </li>
+            </ul>
+          </div>
+         
+
+          {/* <div id="main">
+           <button id = "disappear" class="openbtn" onClick={openNav}>&#9776; </button>
+          </div> */}
+          <ul className="utilities">
+            <li className="users">
+              <Link to="#">{this.props.auth.user.name}</Link>
+            </li>
+            <li className="logout warn">
+              <Link to="/login" onClick={this.onLogoutClick.bind(this)}>
+                Log Out
+              </Link>
+            </li>
           </ul>
-        </nav>
+        </header>
+        <main></main>
       </Fragment>
     );
   }
@@ -79,3 +126,36 @@ export default connect(
   mapStateToProps,
   { logoutUser }
 )(AdminLayout);
+
+// /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+// function openNav() {
+//   document.getElementById("mySidebar").style.width = "250px";
+//   document.getElementById("main").style.marginLeft = "250px";
+//   document.getElementById("disappear").style.display = "none";
+
+// }
+
+// /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+// function closeNav() {
+//   document.getElementById("mySidebar").style.width = "0";
+//   document.getElementById("main").style.marginLeft = "0";
+//   document.getElementById("disappear").style.display = "block";
+
+// }
+
+//     return (
+//       <div>
+//       <div id="mySidebar" className="sidebar">
+//   <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&#9776;</a>
+//   <a href="#">Dashboard</a>
+//   <a href="#">Outcomes</a>
+//   <a href="#">Performance Measures</a>
+//   <a href="#">Assessment Cycles</a>
+//   <a href="#">Evaluator</a>
+// </div>
+  
+
+// <div id="main">
+//   <button id = "disappear" class="openbtn" onClick={openNav}>&#9776; </button>
+// </div>
+// </div>
